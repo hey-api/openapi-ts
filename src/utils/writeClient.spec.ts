07e0240154ec0547aa5c1d3fee429820eeaeeb37
Lubos <lmenus@lmen.us>
2024-03-04 02:27:01 +0000

    refactor(types): allow passing config

--- src/utils/writeClient.spec.ts
+++ src/utils/writeClient.spec.ts
@@ -17,23 +17,24 @@ describe('writeClient', () => {
         };
 
         const templates: Templates = {
-            index: () => 'index',
             client: () => 'client',
+            core: {
+                apiError: () => 'apiError',
+                apiRequestOptions: () => 'apiRequestOptions',
+                apiResult: () => 'apiResult',
+                baseHttpRequest: () => 'baseHttpRequest',
+                cancelablePromise: () => 'cancelablePromise',
+                httpRequest: () => 'httpRequest',
+                request: () => 'request',
+                settings: () => 'settings',
+                types: () => 'types',
+            },
             exports: {
                 model: () => 'model',
                 schema: () => 'schema',
                 service: () => 'service',
             },
-            core: {
-                settings: () => 'settings',
-                apiError: () => 'apiError',
-                apiRequestOptions: () => 'apiRequestOptions',
-                apiResult: () => 'apiResult',
-                cancelablePromise: () => 'cancelablePromise',
-                request: () => 'request',
-                baseHttpRequest: () => 'baseHttpRequest',
-                httpRequest: () => 'httpRequest',
-            },
+            index: () => 'index',
         };
 
         await writeClient(client, templates, {
