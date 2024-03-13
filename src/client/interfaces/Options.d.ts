9b3c93c5c7a5044eebde4da8c2115d297a49f23c
Lubos <lmenus@lmen.us>
2024-03-13 06:08:12 +0000

    fix(api): add option to specify custom base path

--- src/client/interfaces/Options.d.ts
+++ src/client/interfaces/Options.d.ts
@@ -4,23 +4,84 @@ import { Indent } from '../../Indent';
 export type ServiceResponse = 'body' | 'generics' | 'response';
 
 export interface Options {
+    /**
+     * Process generated files with autoformatter
+     */
     autoformat?: boolean;
+    /**
+     * Manually set base in OpenAPI config instead of inferring from server value
+     */
+    base?: string;
+    /**
+     * Custom client class name
+     */
     clientName?: string;
+    /**
+     * Generate core client classes
+     */
     exportCore?: boolean;
+    /**
+     * Generate models
+     */
     exportModels?: boolean | string;
+    /**
+     * Generate schemas
+     */
     exportSchemas?: boolean;
+    /**
+     * Generate services
+     */
     exportServices?: boolean | string;
+    /**
+     * The selected httpClient (fetch, xhr, node or axios)
+     */
     httpClient?: HttpClient;
+    /**
+     * Indentation options (4, 2 or tab)
+     */
     indent?: Indent;
+    /**
+     * The relative location of the OpenAPI spec
+     */
     input: string | Record<string, any>;
+    /**
+     * The relative location of the output directory
+     */
     output: string;
+    /**
+     * Model name postfix
+     */
     postfixModels?: string;
+    /**
+     * Service name postfix
+     */
     postfixServices?: string;
+    /**
+     * Path to custom request file
+     */
     request?: string;
+    /**
+     * Define shape of returned value from service calls
+     */
     serviceResponse?: ServiceResponse;
+    /**
+     * Output Date instead of string for the format "date-time" in the models
+     */
     useDateType?: boolean;
+    /**
+     * Should the operationId be used when generating operation names?
+     */
     useOperationId?: boolean;
+    /**
+     * Use options or arguments functions
+     */
     useOptions?: boolean;
+    /**
+     * Use union types instead of enums
+     */
     useUnionTypes?: boolean;
+    /**
+     * Write the files to disk (true or false)
+     */
     write?: boolean;
 }
--- src/utils/writeClientCore.spec.ts
+++ src/utils/writeClientCore.spec.ts
@@ -11,15 +11,9 @@ import { writeClientCore } from './writeClientCore';
 jest.mock('./fileSystem');
 
 describe('writeClientCore', () => {
-    it('should write to filesystem', async () => {
-        const client: Client = {
-            server: 'http://localhost:8080',
-            version: '1.0',
-            models: [],
-            services: [],
-        };
-
-        const templates: Templates = {
+    let templates: Templates;
+    beforeEach(() => {
+        const _templates: Templates = {
             client: () => 'client',
             core: {
                 apiError: () => 'apiError',
@@ -29,7 +23,7 @@ describe('writeClientCore', () => {
                 cancelablePromise: () => 'cancelablePromise',
                 httpRequest: () => 'httpRequest',
                 request: () => 'request',
-                settings: () => 'settings',
+                settings: jest.fn().mockReturnValue('settings'),
                 types: () => 'types',
             },
             exports: {
@@ -38,6 +32,16 @@ describe('writeClientCore', () => {
                 service: () => 'service',
             },
             index: () => 'index',
+        };
+        templates = _templates;
+    });
+
+    it('should write to filesystem', async () => {
+        const client: Client = {
+            models: [],
+            server: 'http://localhost:8080',
+            services: [],
+            version: '1.0',
         };
 
         await writeClientCore(client, templates, '/', {
@@ -48,12 +52,65 @@ describe('writeClientCore', () => {
             serviceResponse: 'body',
         });
 
-        expect(writeFile).toBeCalledWith(resolve('/', '/OpenAPI.ts'), `settings${EOL}`);
-        expect(writeFile).toBeCalledWith(resolve('/', '/ApiError.ts'), `apiError${EOL}`);
-        expect(writeFile).toBeCalledWith(resolve('/', '/ApiRequestOptions.ts'), `apiRequestOptions${EOL}`);
-        expect(writeFile).toBeCalledWith(resolve('/', '/ApiResult.ts'), `apiResult${EOL}`);
-        expect(writeFile).toBeCalledWith(resolve('/', '/CancelablePromise.ts'), `cancelablePromise${EOL}`);
-        expect(writeFile).toBeCalledWith(resolve('/', '/request.ts'), `request${EOL}`);
-        expect(writeFile).toBeCalledWith(resolve('/', '/types.ts'), `types${EOL}`);
+        expect(writeFile).toHaveBeenCalledWith(resolve('/', '/OpenAPI.ts'), `settings${EOL}`);
+        expect(writeFile).toHaveBeenCalledWith(resolve('/', '/ApiError.ts'), `apiError${EOL}`);
+        expect(writeFile).toHaveBeenCalledWith(resolve('/', '/ApiRequestOptions.ts'), `apiRequestOptions${EOL}`);
+        expect(writeFile).toHaveBeenCalledWith(resolve('/', '/ApiResult.ts'), `apiResult${EOL}`);
+        expect(writeFile).toHaveBeenCalledWith(resolve('/', '/CancelablePromise.ts'), `cancelablePromise${EOL}`);
+        expect(writeFile).toHaveBeenCalledWith(resolve('/', '/request.ts'), `request${EOL}`);
+        expect(writeFile).toHaveBeenCalledWith(resolve('/', '/types.ts'), `types${EOL}`);
+    });
+
+    it('uses client server value for base', async () => {
+        const client: Client = {
+            models: [],
+            server: 'http://localhost:8080',
+            services: [],
+            version: '1.0',
+        };
+
+        await writeClientCore(client, templates, '/', {
+            httpClient: HttpClient.FETCH,
+            indent: Indent.SPACE_4,
+            input: '',
+            output: '',
+            serviceResponse: 'body',
+        });
+
+        expect(templates.core.settings).toHaveBeenCalledWith({
+            clientName: undefined,
+            httpClient: 'fetch',
+            httpRequest: 'FetchHttpRequest',
+            server: 'http://localhost:8080',
+            serviceResponse: 'body',
+            version: '1.0',
+        });
+    });
+
+    it('uses custom value for base', async () => {
+        const client: Client = {
+            models: [],
+            server: 'http://localhost:8080',
+            services: [],
+            version: '1.0',
+        };
+
+        await writeClientCore(client, templates, '/', {
+            base: 'foo',
+            httpClient: HttpClient.FETCH,
+            indent: Indent.SPACE_4,
+            input: '',
+            output: '',
+            serviceResponse: 'body',
+        });
+
+        expect(templates.core.settings).toHaveBeenCalledWith({
+            clientName: undefined,
+            httpClient: 'fetch',
+            httpRequest: 'FetchHttpRequest',
+            server: 'foo',
+            serviceResponse: 'body',
+            version: '1.0',
+        });
     });
 });
