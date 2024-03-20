f707a14ec2f8a63d11df40ff0f49a3412e6e4e15
Jordan Shatford <jordanshatford@live.com>
2024-03-20 14:06:59 +1100

    chore: address comments on pr

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,10 +7,63 @@ import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
-import type { RollupOptions } from 'rollup';
+import handlebars from 'handlebars';
+import type { Plugin, RollupOptions } from 'rollup';
 import { defineConfig } from 'rollup';
 
-import { handlebarsPlugin } from './handlebars';
+/**
+ * Custom plugin to parse handlebar imports and precompile
+ * the template on the fly. This reduces runtime by about
+ * half on large projects.
+ */
+export function handlebarsPlugin(): Plugin {
+    return {
+        name: 'handlebars',
+        resolveId: (file: any, importer: any) => {
+            if (path.extname(file) === '.hbs') {
+                return path.resolve(path.dirname(importer), file);
+            }
+            return null;
+        },
+        load: (file: any) => {
+            if (path.extname(file) === '.hbs') {
+                const template = readFileSync(file, 'utf8').toString().trim();
+                const templateSpec = handlebars.precompile(template, {
+                    knownHelpers: {
+                        camelCase: true,
+                        dataParameters: true,
+                        debugThis: true,
+                        enumKey: true,
+                        enumName: true,
+                        enumUnionType: true,
+                        enumValue: true,
+                        equals: true,
+                        escapeComment: true,
+                        escapeDescription: true,
+                        escapeNewline: true,
+                        exactArray: true,
+                        ifdef: true,
+                        ifOperationDataOptional: true,
+                        intersection: true,
+                        modelImports: true,
+                        modelsExports: true,
+                        modelUnionType: true,
+                        nameOperationDataType: true,
+                        notEquals: true,
+                        operationDataType: true,
+                        useDateType: true,
+                    },
+                    knownHelpersOnly: true,
+                    noEscape: true,
+                    preventIndent: true,
+                    strict: true,
+                });
+                return `export default ${templateSpec};`;
+            }
+            return null;
+        },
+    };
+}
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
--- src/utils/__mocks__/fileSystem.ts
+++
@@ -1,8 +0,0 @@
-import { vi } from 'vitest';
-
-export const readFile = vi.fn();
-export const writeFile = vi.fn();
-export const copyFile = vi.fn();
-export const exists = vi.fn();
-export const rmdir = vi.fn();
-export const mkdir = vi.fn();
