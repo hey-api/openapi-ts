302c43c8c00124a41bb886313255343fe5a76a90
Jordan Shatford <jordanshatford@live.com>
2024-04-04 14:52:47 +1100

    chore(docs): add support for localization

---
+++ docs/.vitepress/config/index.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitepress';
+
+import en from './en';
+import shared from './shared';
+
+export default defineConfig({
+    ...shared,
+    locales: {
+        root: { label: 'English', ...en },
+    },
+});
