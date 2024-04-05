5a40ea70d124bee9a097729941bd70c06bdd08e5
Lubos <lmenus@lmen.us>
2024-04-05 16:27:50 +0100

    chore(models): use TypeScript Compiler API to create models

--- packages/openapi-ts/src/utils/write/models.ts
+++ packages/openapi-ts/src/utils/write/models.ts
@@ -1,9 +1,62 @@
 import { writeFileSync } from 'node:fs';
 import path from 'node:path';
 
+import ts from 'typescript';
+
+import { tsNodeToString } from '../../compiler/utils';
+import type { Model } from '../../openApi';
 import type { Client } from '../../types/client';
 import type { Config } from '../../types/config';
 import type { Templates } from '../handlebars';
+
+const modelToTypeScriptInterface = (config: Config, model: Model) => {
+    const typeDeclaration = ts.factory.createTypeAliasDeclaration(
+        [ts.factory.createModifier(ts.SyntaxKind.ExportKeyword)],
+        ts.factory.createIdentifier('StringType_WIP'),
+        undefined,
+        ts.factory.createTypeReferenceNode('string')
+    );
+
+    const comment = ts.factory.createJSDocComment(ts.factory.createJSDocText('bar').text, [
+        ts.factory.createJSDocTypedefTag(
+            undefined,
+            undefined,
+            ts.factory.createIdentifier('{Object}'),
+            'key2 - Description for key2.'
+        ),
+    ]);
+    const commentTrailingNewLine = ts.factory.createIdentifier('\n');
+
+    switch (model.export) {
+        case 'all-of':
+        case 'any-of':
+        case 'one-of':
+            // {{>exportComposition}}
+            // return compositionSchema(config, model);
+            return [comment, commentTrailingNewLine, typeDeclaration];
+        case 'enum':
+            // {{>exportEnum exportType="true"}}
+            // return enumSchema(config, model);
+            return [comment, commentTrailingNewLine, typeDeclaration];
+        case 'interface':
+            // {{>exportInterface}}
+            // return interfaceSchema(config, model);
+            return [comment, commentTrailingNewLine, typeDeclaration];
+        default:
+            // {{>exportType}}
+            // return genericSchema(config, model);
+            return [comment, commentTrailingNewLine, typeDeclaration];
+    }
+};
+
+const exportModel = (config: Config, model: Model) => {
+    const expression = modelToTypeScriptInterface(config, model);
+    let result: string = '';
+    for (const e of expression) {
+        result += tsNodeToString(e);
+    }
+    return result;
+};
 
 /**
  * Generate Models using the Handlebar template and write to disk.
@@ -22,15 +75,18 @@ export const writeClientModels = async (
         return;
     }
 
-    // Generate a file with all models
-    const results: string[] = [];
+    // TODO: replace array of strings with array of TS nodes, handle double newlines
+    // between them with Compiler, print to string only once.
+    let results: string[] = [];
+
     for (const model of client.models) {
-        const result = templates.exports.model({
+        const result = exportModel(config, model);
+        const resultOld = templates.exports.model({
             $config: config,
             ...model,
         });
-        results.push(result);
+        results = [...results, result, resultOld];
     }
-    const file = path.resolve(outputPath, 'models.ts');
-    writeFileSync(file, results.join('\n\n'));
+
+    writeFileSync(path.resolve(outputPath, 'models.ts'), results.join('\n\n'));
 };
