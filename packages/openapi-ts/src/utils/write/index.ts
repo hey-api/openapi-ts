7067de1b10d3dd455d981de06a071d8700670576
Jordan Shatford <jordanshatford@live.com>
2024-04-05 16:01:24 +1100

    chore(compiler): add typescript file helper class

--- packages/openapi-ts/src/utils/write/index.ts
+++ packages/openapi-ts/src/utils/write/index.ts
@@ -1,7 +1,6 @@
-import { writeFileSync } from 'node:fs';
 import path from 'node:path';
 
-import compiler from '../../compiler';
+import compiler, { TypeScriptFile } from '../../compiler';
 import type { Client } from '../../types/client';
 import type { Config } from '../../types/config';
 
@@ -13,30 +12,30 @@ import type { Config } from '../../types/config';
  * @param config {@link Config} passed to the `createClient()` method
  */
 export const writeClientIndex = async (client: Client, outputPath: string, config: Config): Promise<void> => {
-    const lines: string[] = [];
+    const file = new TypeScriptFile();
     if (config.name) {
-        lines.push(compiler.export.named([config.name], `./${config.name}`));
+        file.push(compiler.export.named([config.name], `./${config.name}`));
     }
     if (config.exportCore) {
-        lines.push(compiler.export.named('ApiError', './core/ApiError'));
+        file.push(compiler.export.named('ApiError', './core/ApiError'));
         if (config.name) {
-            lines.push(compiler.export.named('BaseHttpRequest', './core/BaseHttpRequest'));
+            file.push(compiler.export.named('BaseHttpRequest', './core/BaseHttpRequest'));
         }
         if (config.client !== 'angular') {
-            lines.push(compiler.export.named(['CancelablePromise', 'CancelError'], './core/CancelablePromise'));
+            file.push(compiler.export.named(['CancelablePromise', 'CancelError'], './core/CancelablePromise'));
         }
-        lines.push(compiler.export.named(['OpenAPI', { isTypeOnly: true, name: 'OpenAPIConfig' }], './core/OpenAPI'));
+        file.push(compiler.export.named(['OpenAPI', { isTypeOnly: true, name: 'OpenAPIConfig' }], './core/OpenAPI'));
     }
     if (client.models.length) {
         if (config.exportModels) {
-            lines.push(compiler.export.all('./models'));
+            file.push(compiler.export.all('./models'));
         }
         if (config.exportSchemas) {
-            lines.push(compiler.export.all('./schemas'));
+            file.push(compiler.export.all('./schemas'));
         }
     }
     if (client.services.length && config.exportServices) {
-        lines.push(compiler.export.all('./services'));
+        file.push(compiler.export.all('./services'));
     }
-    await writeFileSync(path.resolve(outputPath, 'index.ts'), lines.join('\n'));
+    file.write(path.resolve(outputPath, 'index.ts'));
 };
