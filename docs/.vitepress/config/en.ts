9964872f0623beea96438e0adb23db4c32ece378
Lubos <lmenus@lmen.us>
2024-04-19 03:49:14 +0100

    docs: add tanstack-query and http clients section

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -10,8 +10,10 @@ export default defineConfig({
                 items: [
                     { text: 'Get Started', link: '/openapi-ts/get-started' },
                     { text: 'Configuration', link: '/openapi-ts/configuration' },
+                    { text: 'Clients <span class="soon">soon</span>', link: '/openapi-ts/clients' },
                     { text: 'Interceptors', link: '/openapi-ts/interceptors' },
                     { text: 'Integrations <span class="soon">soon</span>', link: '/openapi-ts/integrations' },
+                    { text: 'TanStack Query <span class="soon">soon</span>', link: '/openapi-ts/tanstack-query' },
                     { text: 'Migrating', link: '/openapi-ts/migrating' },
                 ],
             },
---
+++ docs/openapi-ts/clients.md
@@ -0,0 +1,10 @@
+---
+title: Clients
+description: Generate query keys and query functions. Use them with our REST clients or bring your own.
+---
+
+# REST Clients <span class="soon">Soon</span>
+
+We all send HTTP requests in a slightly different way. Hey API doesn't force you to use any specific technology. What we do, however, is support your choice with great clients. All seamlessly integrated with our other features.
+
+[Next.js](https://nextjs.org/), [Axios](https://axios-http.com/), and [Fetch API](https://developer.mozilla.org/docs/Web/API/Fetch_API) coming soon.
