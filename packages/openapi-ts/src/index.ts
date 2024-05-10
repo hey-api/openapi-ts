ad6e76e9b11835ecf4205edc5fa3054e3cb75d37
Maximilian Dewald <maximilian.dewald@envelio.de>
2024-05-10 13:11:58 +0200

    prettier fix

--- packages/openapi-ts/src/index.ts
+++ packages/openapi-ts/src/index.ts
@@ -200,10 +200,14 @@ const getTypes = (userConfig: UserConfig): Config['types'] => {
 
 const getInstalledDependencies = (): Dependencies => {
   const toReducedDependencies = (p: PackageDependencies): Dependencies =>
-    [p.dependencies ?? {}, p.devDependencies ?? {}, p.peerDependencies ?? {}].reduce(
+    [
+      p.dependencies ?? {},
+      p.devDependencies ?? {},
+      p.peerDependencies ?? {},
+    ].reduce(
       (acc, deps) => ({
         ...acc,
-        ...deps
+        ...deps,
       }),
       {},
     );
