6ce441fb1be5f4180d0b12f861d95a5481f50a81
Lubos <lmenus@lmen.us>
2024-08-11 03:06:50 +0100

    fix: export more types to resolve TypeScript errors

--- examples/openapi-ts-tanstack-vue-query/src/stores/counter.ts
+++ examples/openapi-ts-tanstack-vue-query/src/stores/counter.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { computed,ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
