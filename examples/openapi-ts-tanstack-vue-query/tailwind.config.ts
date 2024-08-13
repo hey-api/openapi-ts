887c8fb4ff05e317ae01130ba02e42156cc0cc37
Mads Hougesen <mads@mhouge.dk>
2024-08-13 18:45:15 +0200

    build: install tailwind

---
+++ examples/openapi-ts-tanstack-vue-query/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  plugins: [],
+  theme: {
+    extend: {}
+  }
+} satisfies Config
