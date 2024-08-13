42af4cd33d74792b129677a410d94ebb4d5abce8
Mads Hougesen <mads@mhouge.dk>
2024-08-13 18:47:51 +0200

    chore: remove unnecessary code

--- examples/openapi-ts-tanstack-vue-query/src/router/index.ts
+++ examples/openapi-ts-tanstack-vue-query/src/router/index.ts
@@ -1,22 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from '../views/HomeView.vue'
+import TanstackExample from '@/views/TanstackExample.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      component: HomeView,
+      component: TanstackExample,
       name: 'home',
       path: '/'
-    },
-    {
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-      name: 'about',
-      path: '/about'
     }
   ]
 })
