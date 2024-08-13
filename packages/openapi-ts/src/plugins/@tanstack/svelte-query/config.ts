851ca3ce2d8eb8f5bf5e781406807177ff6ef7ab
Lubos <lmenus@lmen.us>
2024-08-13 14:02:52 +0100

    chore: progress on TanStack Query plugin

---
+++ packages/openapi-ts/src/plugins/@tanstack/svelte-query/config.ts
@@ -0,0 +1,36 @@
+export interface PluginTanStackSvelteQuery {
+  /**
+   * Generate `createInfiniteQuery()` helpers? These will be generated from GET and POST requests where a pagination parameter is detected.
+   * @default true
+   */
+  infiniteQueryOptions?: boolean;
+  /**
+   * Generate {@link https://tanstack.com/query/v5/docs/framework/svelte/reference/functions/createmutation `createMutation()`} helpers? These will be generated from DELETE, PATCH, POST, and PUT requests.
+   * @default true
+   */
+  mutationOptions?: boolean;
+  /**
+   * Generate TanStack Svelte Query output from the provided input.
+   */
+  name: '@tanstack/svelte-query';
+  /**
+   * Name of the generated file.
+   * @default '@tanstack/svelte-query'
+   */
+  output?: string;
+  /**
+   * Generate {@link https://tanstack.com/query/v5/docs/framework/svelte/reference/functions/createquery `createQuery()`} helpers?
+   * These will be generated from all requests.
+   * @default true
+   */
+  queryOptions?: boolean;
+}
+
+export const pluginTanStackSvelteQueryDefaultConfig: Required<PluginTanStackSvelteQuery> =
+  {
+    infiniteQueryOptions: true,
+    mutationOptions: true,
+    name: '@tanstack/svelte-query',
+    output: '@tanstack/svelte-query',
+    queryOptions: true,
+  };
