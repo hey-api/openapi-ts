6d60c481ac8cc10ca576859aff411b4458aecca7
Mads Hougesen <mads@mhouge.dk>
2024-08-13 19:54:44 +0200

    docs: @tanstack/vue-query vue example

--- examples/openapi-ts-tanstack-vue-query/tailwind.config.ts
+++ examples/openapi-ts-tanstack-vue-query/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   plugins: [],
   theme: {
     extend: {}
