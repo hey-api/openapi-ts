5dacbe0085e79742f8448ad341fd5d41015dccc5
Vitaly Kuprin <vitas_s@inbox.lv>
2024-09-12 23:44:46 +0200

    feat: add tests for duplicate nulls check

--- packages/openapi-ts/src/generate/__tests__/schemas.spec.ts
+++ packages/openapi-ts/src/generate/__tests__/schemas.spec.ts
@@ -114,4 +114,66 @@ describe('generateSchemas', () => {
 
     expect(nameFn).toHaveBeenCalledWith('foo', schema);
   });
+
+  it('removes duplicate nulls in oneOf union types', async () => {
+    setConfig({
+      client: {
+        name: 'legacy/fetch',
+      },
+      configFile: '',
+      debug: false,
+      dryRun: false,
+      experimental_parser: false,
+      exportCore: true,
+      input: '',
+      name: 'AppClient',
+      output: {
+        path: '',
+      },
+      plugins: [],
+      schemas: {
+        export: true,
+      },
+      services: {},
+      types: {
+        enums: 'javascript',
+      },
+      useOptions: true,
+    });
+
+    const schemaWithDuplicateNulls: OpenApiV3Schema = {
+      nullable: true,
+      oneOf: [
+        { type: 'string' },
+        { nullable: true, type: 'string' },
+        null as unknown as OpenApiV3Schema,
+      ],
+    };
+
+    if ('openapi' in openApi) {
+      openApi.components = {
+        schemas: {
+          foo: schemaWithDuplicateNulls,
+        },
+      };
+    }
+
+    const files: Files = {};
+
+    await generateSchemas({ files, openApi });
+
+    files.schemas.write();
+
+    expect(writeFileSync).toHaveBeenCalledWith(
+      expect.stringContaining(path.resolve('schemas.gen.ts')),
+      expect.stringContaining(`oneOf: [`),
+    );
+
+    expect(writeFileSync).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.stringMatching(
+        /oneOf:\s*\[\s*{\s*type:\s*'string'\s*},\s*{\s*type:\s*'string',\s*nullable:\s*true\s*}\s*]/,
+      ),
+    );
+  });
 });
