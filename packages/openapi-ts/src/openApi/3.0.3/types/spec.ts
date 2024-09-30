76779246534391deca5a371df2c7dc76e9d2eb73
Lubos <lmenus@lmen.us>
2024-09-30 19:29:46 +0800

    fix: handle multiple form-data parameters in Swagger 2.0

---
+++ packages/openapi-ts/src/openApi/3.0.3/types/spec.ts
@@ -0,0 +1,7 @@
+export interface OpenApiV3_0_3 {
+  /**
+   * **REQUIRED**. This string MUST be the {@link https://semver.org/spec/v2.0.0.html semantic version number} of the {@link https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.0.3.md#versions OpenAPI Specification version} that the OpenAPI document uses. The `openapi` field SHOULD be used by tooling specifications and clients to interpret the OpenAPI document. This is _not_ related to the API {@link https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.0.3.md#infoVersion `info.version`} string.
+   */
+  openapi: '3.0.3';
+  // TODO
+}
