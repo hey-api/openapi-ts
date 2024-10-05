032338c47506ccfd567bbbf915398942c8394bc2
Lubos <lmenus@lmen.us>
2024-10-06 00:09:03 +0800

    fix: use correct relative path to bundled client when imported from nested module

--- packages/openapi-ts/src/generate/utils.ts
+++ packages/openapi-ts/src/generate/utils.ts
@@ -5,3 +5,30 @@ export const ensureDirSync = (path: PathLike) => {
     mkdirSync(path, { recursive: true });
   }
 };
+
+/**
+ * Construct a relative import path to modules. This is used for example
+ * in plugins to import types or services module.
+ */
+export const relativeModulePath = ({
+  moduleOutput,
+  sourceOutput,
+}: {
+  /**
+   * Output path to the imported module.
+   * @example
+   * 'types'
+   */
+  moduleOutput: string;
+  /**
+   * Output path to the source module.
+   * @example
+   * '@tanstack/react-query'
+   */
+  sourceOutput: string;
+}): string => {
+  const outputParts = sourceOutput.split('/');
+  const relativePath =
+    new Array(outputParts.length).fill('').join('../') || './';
+  return `${relativePath}${moduleOutput}`;
+};
