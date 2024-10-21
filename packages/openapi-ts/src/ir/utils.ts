97cf29feca56e488fb9f16fbfd223157d6bd1a91
Lubos <lmenus@lmen.us>
2024-10-21 16:33:54 +0800

    refactor: polish parser output

--- packages/openapi-ts/src/ir/utils.ts
+++ packages/openapi-ts/src/ir/utils.ts
@@ -6,20 +6,39 @@ import type { IRSchemaObject } from './ir';
  */
 export const addItemsToSchema = ({
   items,
+  logicalOperator = 'or',
+  mutateSchemaOneItem = false,
   schema,
 }: {
   items: Array<IRSchemaObject>;
+  logicalOperator?: IRSchemaObject['logicalOperator'];
+  mutateSchemaOneItem?: boolean;
   schema: IRSchemaObject;
 }) => {
   if (!items.length) {
-    return;
+    return schema;
+  }
+
+  if (schema.type === 'tuple') {
+    schema.items = items;
+    return schema;
+  }
+
+  if (items.length !== 1) {
+    schema.items = items;
+    schema.logicalOperator = logicalOperator;
+    return schema;
+  }
+
+  if (mutateSchemaOneItem) {
+    // bring composition up to avoid extraneous brackets
+    schema = {
+      ...schema,
+      ...items[0],
+    };
+    return schema;
   }
 
   schema.items = items;
-
-  if (items.length === 1 || schema.type === 'tuple') {
-    return;
-  }
-
-  schema.logicalOperator = 'or';
+  return schema;
 };
