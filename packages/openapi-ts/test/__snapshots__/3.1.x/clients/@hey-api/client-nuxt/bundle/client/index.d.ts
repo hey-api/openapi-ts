0d90910fe2522576260a5659126f50ca914e2c89
Lubos <lmenus@lmen.us>
2025-02-09 11:26:10 +0000

    chore: update snapshots

--- packages/openapi-ts/test/__snapshots__/3.1.x/clients/@hey-api/client-nuxt/bundle/client/index.d.ts
+++ packages/openapi-ts/test/__snapshots__/3.1.x/clients/@hey-api/client-nuxt/bundle/client/index.d.ts
@@ -28,15 +28,15 @@ interface SerializerOptions<T> {
 type ArrayStyle = 'form' | 'spaceDelimited' | 'pipeDelimited';
 type ObjectStyle = 'form' | 'deepObject';
 
-type QuerySerializer$1 = (query: Record<string, unknown>) => string;
 type BodySerializer = (body: any) => any;
+type QuerySerializer$1 = (query: Record<string, unknown>) => string;
 interface QuerySerializerOptions {
     allowReserved?: boolean;
     array?: SerializerOptions<ArrayStyle>;
     object?: SerializerOptions<ObjectStyle>;
 }
 declare const formDataBodySerializer: {
-    bodySerializer: <T extends Record<string, any> | Array<Record<string, any>>>(body: T) => FormData;
+    bodySerializer: <T>(body: T) => FormData;
 };
 declare const jsonBodySerializer: {
     bodySerializer: <T>(body: T) => string;
