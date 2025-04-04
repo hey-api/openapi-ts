991878bb19d497f42b46b0ebda196143453a0bc4
Landon Gavin <hi@landongavin.com>
2025-04-04 18:11:41 -0400

    feat(vitest): add workspace configuration for Vitest to support multiple config file types

---
+++ vitest.workspace.js
@@ -0,0 +1,14 @@
+import { defineWorkspace } from 'vitest/config';
+
+export default defineWorkspace([
+  '**/vite.config.ts',
+  '**/vite.config.mts',
+  '**/vite.config.js',
+  '**/vite.config.mjs',
+  '**/vite.config.cjs',
+  '**/vitest.config.ts',
+  '**/vitest.config.mts',
+  '**/vitest.config.js',
+  '**/vitest.config.mjs',
+  '**/vitest.config.cjs',
+]);
