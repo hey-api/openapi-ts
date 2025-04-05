8bd0ebff262ad8f191fee7b95bed82db9f1ca4d8
Landon <hi@landongavin.com>
2025-04-05 15:57:20 -0400

    feat(nx-plugin): add force option to update executor and move compare spec

--- packages/nx-plugin/src/executors/update-api/schema.d.ts
+++ packages/nx-plugin/src/executors/update-api/schema.d.ts
@@ -1,6 +1,10 @@
 export interface UpdateApiExecutorSchema {
   client: string;
   directory: string;
+  /**
+   * If true, the Client code will be regenerated even if the spec has not changed
+   */
+  force?: boolean;
   name: string;
   plugins: string[];
   scope: string;
