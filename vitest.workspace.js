800b8a02e304c5873475d072e613963c55c5188d
Landon Gavin <hi@landongavin.com>
2025-04-10 00:26:56 -0400

    feat(vitest): streamline workspace configuration

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,14 +1,3 @@
 import { defineWorkspace } from 'vitest/config';
 
-export default defineWorkspace([
-  '**/vite.config.ts',
-  '**/vite.config.mts',
-  '**/vite.config.js',
-  '**/vite.config.mjs',
-  '**/vite.config.cjs',
-  '**/vitest.config.ts',
-  '**/vitest.config.mts',
-  '**/vitest.config.js',
-  '**/vitest.config.mjs',
-  '**/vitest.config.cjs',
-]);
+export default defineWorkspace(['./packages/*/vitest.config.ts']);
