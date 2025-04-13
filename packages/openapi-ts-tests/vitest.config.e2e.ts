f4291d2af253cc632f8f905949ae41997ad4f5c8
Landon <hi@landongavin.com>
2025-04-13 18:46:27 -0400

    feat(test): add base vite(st) configs

--- packages/openapi-ts-tests/vitest.config.e2e.ts
+++ packages/openapi-ts-tests/vitest.config.e2e.ts
@@ -1,14 +1,15 @@
 import { fileURLToPath } from 'node:url';
 
-import { defineConfig } from 'vitest/config';
+import { createVitestConfig } from '@config/vite-base';
 
-export default defineConfig({
-  plugins: [],
-  test: {
-    // Dont run tests in parallel. This is to ensure the test server can start up.
-    // And that the port was not previously taken.
-    fileParallelism: false,
-    include: ['test/e2e/**/*.test.ts'],
-    root: fileURLToPath(new URL('./', import.meta.url)),
+export default createVitestConfig(
+  fileURLToPath(new URL('./', import.meta.url)),
+  {
+    test: {
+      // Dont run tests in parallel. This is to ensure the test server can start up.
+      // And that the port was not previously taken.
+      fileParallelism: false,
+      include: ['test/e2e/**/*.test.ts'],
+    },
   },
-});
+);
