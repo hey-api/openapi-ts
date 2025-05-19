373e15a34fd81ac596a0797ddb133a47aeceee1f
Lubos <lmenus@lmen.us>
2025-05-19 15:09:28 -0400

    fix(valibot): add valibot plugin

---
+++ .changeset/three-lobsters-grow.md
@@ -0,0 +1,5 @@
+---
+'@hey-api/openapi-ts': patch
+---
+
+fix(valibot): add valibot plugin
---
+++ packages/openapi-ts/src/plugins/valibot/config.ts
@@ -0,0 +1,20 @@
+import type { Plugin } from '../types';
+import { handler } from './plugin';
+import type { Config } from './types';
+
+export const defaultConfig: Plugin.Config<Config> = {
+  _handler: handler,
+  _handlerLegacy: () => {},
+  _tags: ['validator'],
+  exportFromIndex: false,
+  name: 'valibot',
+  output: 'valibot',
+};
+
+/**
+ * Type helper for Valibot plugin, returns {@link Plugin.Config} object
+ */
+export const defineConfig: Plugin.DefineConfig<Config> = (config) => ({
+  ...defaultConfig,
+  ...config,
+});
