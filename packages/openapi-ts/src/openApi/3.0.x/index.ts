302ea359459fb3488ecb09555c2192eeb8266a71
Lubos <lmenus@lmen.us>
2025-06-05 15:10:07 +0900

    fix(parser): add parameters, requestBodies, and responses to supported input.patch fields

--- packages/openapi-ts/src/openApi/3.0.x/index.ts
+++ packages/openapi-ts/src/openApi/3.0.x/index.ts
@@ -1,2 +1,18 @@
 export { parseV3_0_X } from './parser';
 export type { OpenApiV3_0_X } from './types/spec';
+
+import type {
+  ParameterObject,
+  ReferenceObject,
+  RequestBodyObject,
+  ResponseObject,
+  SchemaObject,
+} from './types/spec';
+
+export interface OpenApiV3_0_XTypes {
+  ParameterObject: ParameterObject;
+  ReferenceObject: ReferenceObject;
+  RequestBodyObject: RequestBodyObject;
+  ResponseObject: ResponseObject;
+  SchemaObject: SchemaObject;
+}
