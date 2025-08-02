8e9b9aea00a9428aae7fb012a500d9eb19724304
Idan Ben Ami <idbenami27@gmail.com>
2025-08-02 16:20:29 +0300

    feat(client-axios): enhance createClient to accept AxiosStatic or AxiosInstance

--- packages/openapi-ts/src/plugins/@hey-api/client-axios/__tests__/client.test.ts
+++ packages/openapi-ts/src/plugins/@hey-api/client-axios/__tests__/client.test.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { describe, expect, it } from 'vitest';
 
 import { createClient } from '../bundle/client';
@@ -46,5 +47,39 @@ describe('buildUrl', () => {
 
   it.each(scenarios)('returns $url', ({ options, url }) => {
     expect(client.buildUrl(options)).toBe(url);
+  });
+});
+
+describe('AxiosInstance', () => {
+  it('should create an AxiosInstance if no axios option passed', () => {
+    const client = createClient({ baseURL: 'test-url' });
+
+    expect(client.instance).toBeDefined();
+    expect(client.instance.defaults).toBeDefined();
+    expect(client.instance.defaults.baseURL).toBe('test-url');
+  });
+
+  it('should use the provided AxiosStatic if axios option passed', () => {
+    axios.defaults.baseURL = 'test-url';
+
+    const client = createClient({ axios });
+
+    expect(client.instance).toBeDefined();
+    expect(client.instance.defaults).toBeDefined();
+    expect(client.instance.defaults.baseURL).toBe('test-url');
+  });
+
+  it('should use the provided AxiosInstance if axios option is passed', () => {
+    const axiosInstance = axios.create({ baseURL: 'test-url' });
+
+    axiosInstance.interceptors.request.use((config) => config);
+
+    const client = createClient({ axios: axiosInstance });
+
+    expect(client.instance).toBe(axiosInstance);
+    expect(client.instance.defaults.baseURL).toBe('test-url');
+    expect((client.instance.interceptors.request as any).handlers.length).toBe(
+      1,
+    );
   });
 });
