131b4e9f3459a9e606d8f32dfe7d52c8e3935b8d
Lubos <lubos@heyapi.dev>
2025-08-17 20:26:13 +0800

    docs: fix versioned clients and plugins

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,27 +1,12 @@
-import path from 'node:path';
-
 import { defineConfig } from 'vitepress';
 
-import en from './en';
-import shared from './shared';
+import en from './en.js';
+import shared from './shared.js';
 
 export default defineConfig({
   ...shared,
   locales: {
     ...shared.locales,
     root: { label: 'English', ...en },
-  },
-  vite: {
-    ...shared.vite,
-    resolve: {
-      ...shared.vite?.resolve,
-      alias: {
-        ...shared.vite?.resolve?.alias,
-        '@components': path.resolve(__dirname, '..', 'theme', 'components'),
-        '@data': path.resolve(__dirname, '..', '..', 'data'),
-        '@versions': path.resolve(__dirname, '..', 'theme', 'versions'),
-      },
-      preserveSymlinks: true,
-    },
   },
 });
