abb1b40d7cf9523012e3f774b9264ed56372fe38
dracomithril <dracomithril@gmail.com>
2025-08-19 09:13:47 +0200

    add multiple headers in different type

--- packages/openapi-ts-tests/main/test/__snapshots__/3.1.x/clients/@hey-api/client-next/base-url-false/client/utils.gen.ts
+++ packages/openapi-ts-tests/main/test/__snapshots__/3.1.x/clients/@hey-api/client-next/base-url-false/client/utils.gen.ts
@@ -188,6 +188,25 @@ export const getParseAs = (
   return;
 };
 
+const checkForExistence = (
+  options: Pick<RequestOptions, 'auth' | 'query'> & {
+    headers: Headers;
+  },
+  name?: string,
+): boolean => {
+  if (!name) {
+    return false;
+  }
+  if (
+    options.headers.has(name) ||
+    options.query?.[name] ||
+    options.headers.get('Cookie')?.includes(`${name}=`)
+  ) {
+    return true;
+  }
+  return false;
+};
+
 export const setAuthParams = async ({
   security,
   ...options
@@ -196,6 +215,9 @@ export const setAuthParams = async ({
     headers: Headers;
   }) => {
   for (const auth of security) {
+    if (checkForExistence(options, auth.name)) {
+      continue;
+    }
     const token = await getAuthToken(auth, options.auth);
 
     if (!token) {
@@ -219,8 +241,6 @@ export const setAuthParams = async ({
         options.headers.set(name, token);
         break;
     }
-
-    return;
   }
 };
 
