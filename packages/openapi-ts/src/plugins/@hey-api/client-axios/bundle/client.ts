022aa288a9f3acf1cbd7d6d7c14f82ea68ad4909
Lubos <lubos@heyapi.dev>
2025-10-20 15:02:43 +0800

    Revert "Merge pull request #2763 from mauriciabad/patch-1"
    This reverts commit 670564653bd677e35cc7433c07c5a3e10551bb03, reversing
    changes made to 4bd489c272c20f7c122adc63da38ee1af7a2ebee.

--- packages/openapi-ts/src/plugins/@hey-api/client-axios/bundle/client.ts
+++ packages/openapi-ts/src/plugins/@hey-api/client-axios/bundle/client.ts
@@ -1,5 +1,5 @@
-import type { AxiosInstance, RawAxiosRequestHeaders } from 'axios';
-import axios, { AxiosError } from 'axios';
+import type { AxiosError, AxiosInstance, RawAxiosRequestHeaders } from 'axios';
+import axios from 'axios';
 
 import { createSseClient } from '../../client-core/bundle/serverSentEvents';
 import type { HttpMethod } from '../../client-core/bundle/types';
@@ -67,6 +67,7 @@ export const createClient = (config: Config = {}): Client => {
     return { opts, url };
   };
 
+  // @ts-expect-error
   const request: Client['request'] = async (options) => {
     // @ts-expect-error
     const { opts, url } = await beforeRequest(options);
@@ -84,7 +85,6 @@ export const createClient = (config: Config = {}): Client => {
         params: opts.paramsSerializer ? opts.query : undefined,
         url,
       });
-      if (response instanceof Error) throw response;
 
       let { data } = response;
 
@@ -103,22 +103,13 @@ export const createClient = (config: Config = {}): Client => {
         data: data ?? {},
       };
     } catch (error) {
+      const e = error as AxiosError;
       if (opts.throwOnError) {
-        throw error;
+        throw e;
       }
-
-      if (error instanceof AxiosError) {
-        // @ts-expect-error
-        error.error = error.response?.data ?? {};
-        return error;
-      }
-
-      if (typeof error === 'object' && error !== null) {
-        error.error = {};
-        return error;
-      }
-
-      return { error: {} };
+      // @ts-expect-error
+      e.error = e.response?.data ?? {};
+      return e;
     }
   };
 
