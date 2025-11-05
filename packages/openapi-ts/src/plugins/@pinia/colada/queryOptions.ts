455cf4b1dcc5b8d78f0d19fc7d4a8ca87dc0c8ad
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-11-05 16:26:16 +0300

    feat: direct return awaitSdkExpression when responseStyle === 'response' (@pina)

--- packages/openapi-ts/src/plugins/@pinia/colada/queryOptions.ts
+++ packages/openapi-ts/src/plugins/@pinia/colada/queryOptions.ts
@@ -114,7 +114,7 @@ export const createQueryOptions = ({
   });
 
   const statements: Array<ts.Statement> = [];
-  if (plugin.getPluginOrThrow('@hey-api/sdk').config.responseStyle === 'data') {
+  if (plugin.getPluginOrThrow('@hey-api/sdk').config.responseStyle === 'data' || plugin.getPluginOrThrow('@hey-api/sdk').config.responseStyle === 'response') {
     statements.push(
       tsc.returnVariable({
         expression: awaitSdkExpression,
