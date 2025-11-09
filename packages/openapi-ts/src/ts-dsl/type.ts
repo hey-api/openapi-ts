2e1f96bdceb6fa4c9b2609f178b586789d275373
Lubos <lubos@heyapi.dev>
2025-11-10 01:02:07 +0800

    refactor: move type expr class

--- packages/openapi-ts/src/ts-dsl/type.ts
+++ packages/openapi-ts/src/ts-dsl/type.ts
@@ -7,31 +7,9 @@ import { LiteralTsDsl } from './literal';
 import { mixin } from './mixins/apply';
 import { GenericsMixin } from './mixins/generics';
 import { OptionalMixin } from './mixins/optional';
+import { TypeExprTsDsl } from './type/expr';
 
 export type TypeInput = WithString<boolean | MaybeTsDsl<ts.TypeNode>>;
-
-export abstract class BaseTypeTsDsl<
-  T extends ts.TypeNode | ts.TypeParameterDeclaration,
-> extends TsDsl<T> {
-  protected base?: ts.EntityName;
-  protected constraint?: TypeInput;
-  protected defaultValue?: TypeInput;
-
-  constructor(base?: WithString<ts.Identifier>) {
-    super();
-    if (base) this.base = this.$expr(base);
-  }
-
-  default(value: TypeInput): this {
-    this.defaultValue = value;
-    return this;
-  }
-
-  extends(constraint: TypeInput): this {
-    this.constraint = constraint;
-    return this;
-  }
-}
 
 export class TypeLiteralTsDsl extends TsDsl<ts.LiteralTypeNode> {
   private literal: LiteralTsDsl;
@@ -67,13 +45,28 @@ export class TypeObjectTsDsl extends TsDsl<ts.TypeLiteralNode> {
   }
 }
 
-export class TypeParamTsDsl extends BaseTypeTsDsl<ts.TypeParameterDeclaration> {
+export class TypeParamTsDsl extends TsDsl<ts.TypeParameterDeclaration> {
+  protected base?: ts.EntityName;
+  protected constraint?: TypeInput;
+  protected defaultValue?: TypeInput;
+
   constructor(
     name?: WithString<ts.Identifier>,
     fn?: (base: TypeParamTsDsl) => void,
   ) {
-    super(name);
+    super();
+    if (name) this.base = this.$expr(name);
     fn?.(this);
+  }
+
+  default(value: TypeInput): this {
+    this.defaultValue = value;
+    return this;
+  }
+
+  extends(constraint: TypeInput): this {
+    this.constraint = constraint;
+    return this;
   }
 
   $render(): ts.TypeParameterDeclaration {
@@ -127,49 +120,20 @@ export class TypePropTsDsl extends TsDsl<ts.TypeElement> {
 export interface TypePropTsDsl extends OptionalMixin {}
 mixin(TypePropTsDsl, OptionalMixin);
 
-export class TypeReferenceTsDsl extends BaseTypeTsDsl<ts.TypeNode> {
-  private _object?: TypeObjectTsDsl;
+const base = {
+  literal: (...args: ConstructorParameters<typeof TypeLiteralTsDsl>) =>
+    new TypeLiteralTsDsl(...args),
+  object: (...args: ConstructorParameters<typeof TypeObjectTsDsl>) =>
+    new TypeObjectTsDsl(...args),
+  param: (...args: ConstructorParameters<typeof TypeParamTsDsl>) =>
+    new TypeParamTsDsl(...args),
+  ref: (...args: ConstructorParameters<typeof TypeExprTsDsl>) =>
+    new TypeExprTsDsl(...args),
+};
 
-  constructor(
-    name?: WithString<ts.Identifier>,
-    fn?: (base: TypeReferenceTsDsl) => void,
-  ) {
-    super(name);
-    fn?.(this);
-  }
-
-  /** Starts an object type literal (e.g. `{ foo: string }`). */
-  object(fn?: (o: TypeObjectTsDsl) => void): this {
-    this._object = new TypeObjectTsDsl(fn);
-    return this;
-  }
-
-  $render(): ts.TypeNode {
-    if (this._object) return this.$node(this._object);
-    if (!this.base) throw new Error('Missing base type');
-    const types = this._generics?.map((arg) => this.$type(arg));
-    return ts.factory.createTypeReferenceNode(
-      this.base,
-      // @ts-expect-error --- generics are not officially supported on type references yet
-      types,
-    );
-  }
-}
-
-export interface TypeReferenceTsDsl extends GenericsMixin {}
-mixin(TypeReferenceTsDsl, GenericsMixin);
-
+/** Creates a general expression node. */
 export const TypeTsDsl = Object.assign(
-  (...args: ConstructorParameters<typeof TypeReferenceTsDsl>) =>
-    new TypeReferenceTsDsl(...args),
-  {
-    literal: (...args: ConstructorParameters<typeof TypeLiteralTsDsl>) =>
-      new TypeLiteralTsDsl(...args),
-    object: (...args: ConstructorParameters<typeof TypeObjectTsDsl>) =>
-      new TypeObjectTsDsl(...args),
-    param: (...args: ConstructorParameters<typeof TypeParamTsDsl>) =>
-      new TypeParamTsDsl(...args),
-    ref: (...args: ConstructorParameters<typeof TypeReferenceTsDsl>) =>
-      new TypeReferenceTsDsl(...args),
-  },
+  (...args: ConstructorParameters<typeof TypeExprTsDsl>) =>
+    new TypeExprTsDsl(...args),
+  base,
 );
