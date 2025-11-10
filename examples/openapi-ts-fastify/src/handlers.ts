7b4349f06bffdb8a17c21e04e39713befcc15106
Pewww <jung22337@naver.com>
2025-11-10 18:02:05 +0900

    fix: update snapshot tests

--- examples/openapi-ts-fastify/src/handlers.ts
+++ examples/openapi-ts-fastify/src/handlers.ts
@@ -4,6 +4,7 @@ export const serviceHandlers: RouteHandlers = {
   createPets(request, reply) {
     reply.code(201).send();
   },
+
   listPets(request, reply) {
     reply.code(200).send([]);
   },
