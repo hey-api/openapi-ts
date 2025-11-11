480bf33f588e5741075bf831ac9e217f1cc6a32b
Novak Antonijevic <antonijevic1999@gmail.com>
2025-11-11 20:48:00 +0100

    Add type annotations to queryOptions for TanStack Query
    - Extended CallTsDsl to support type arguments via typeArgs() method
    - Updated queryOptions.ts to include type parameters:
      - TQueryFnData: response type
      - TError: error type
      - TData: response type
      - TQueryKey: ReturnType<typeof queryKeyFunction>
    - Generated queryOptions now have proper type annotations

--- packages/openapi-ts/src/ts-dsl/call.ts
+++ packages/openapi-ts/src/ts-dsl/call.ts
@@ -9,6 +9,7 @@ import { ArgsMixin } from './mixins/args';
 
 export class CallTsDsl extends TsDsl<ts.CallExpression> {
   private _callee: MaybeTsDsl<WithString>;
+  private _typeArguments?: ReadonlyArray<ts.TypeNode>;
 
   constructor(
     callee: MaybeTsDsl<WithString>,
@@ -19,10 +20,18 @@ export class CallTsDsl extends TsDsl<ts.CallExpression> {
     this.args(...args);
   }
 
+  /** Adds type arguments to the call expression (e.g. `fn<string, number>()`). */
+  typeArgs(...args: ReadonlyArray<string | ts.TypeNode>): this {
+    this._typeArguments = args.map((arg) =>
+      typeof arg === 'string' ? (this.$type(arg) as ts.TypeNode) : arg,
+    );
+    return this;
+  }
+
   $render(): ts.CallExpression {
     return ts.factory.createCallExpression(
       this.$node(this._callee),
-      undefined,
+      this._typeArguments,
       this.$args(),
     );
   }
