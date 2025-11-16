74deb5e7f9b300b63c9dfe341f70b3272cda5878
Lubos <lubos@heyapi.dev>
2025-11-16 16:01:38 +0800

    refactor: tanstack dsl

--- packages/openapi-ts-tests/main/test/__snapshots__/3.0.x/internal-name-conflict/@tanstack/react-query.gen.ts
+++ packages/openapi-ts-tests/main/test/__snapshots__/3.0.x/internal-name-conflict/@tanstack/react-query.gen.ts
@@ -36,9 +36,7 @@ const createQueryKey = <TOptions extends Options>(id: string, options?: TOptions
     if (options?.query) {
         params.query = options.query;
     }
-    return [
-        params
-    ];
+    return [params];
 };
 
 export const createQueryKey2 = (options?: Options<CreateData>) => createQueryKey("create", options);
