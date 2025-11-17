5ee8c97570f8f3c20ba3d5380d79485476015dbc
Lubos <lubos@heyapi.dev>
2025-11-17 23:52:33 +0800

    refactor: fastify plugin use dsl

--- packages/openapi-ts/src/ts-dsl/type/object.ts
+++ packages/openapi-ts/src/ts-dsl/type/object.ts
@@ -1,12 +1,23 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type, @typescript-eslint/no-unsafe-declaration-merging */
 import ts from 'typescript';
 
+import type { MaybeTsDsl } from '../base';
 import { TypeTsDsl } from '../base';
 import { mixin } from '../mixins/apply';
 import { OptionalMixin } from '../mixins/optional';
 
 export class TypeObjectTsDsl extends TypeTsDsl<ts.TypeNode> {
   private props: Array<TypePropTsDsl> = [];
+
+  /** Returns true if object has at least one property or spread. */
+  hasProps(): boolean {
+    return this.props.length > 0;
+  }
+
+  /** Returns true if object has no properties or spreads. */
+  get isEmpty(): boolean {
+    return !this.props.length;
+  }
 
   /** Adds a property signature (returns property builder). */
   prop(name: string, fn: (p: TypePropTsDsl) => void): this {
@@ -22,7 +33,7 @@ export class TypeObjectTsDsl extends TypeTsDsl<ts.TypeNode> {
 
 class TypePropTsDsl extends TypeTsDsl<ts.TypeElement> {
   private name: string;
-  private typeInput?: string | ts.TypeNode;
+  private _type?: string | MaybeTsDsl<ts.TypeNode>;
 
   constructor(name: string, fn: (p: TypePropTsDsl) => void) {
     super();
@@ -31,21 +42,21 @@ class TypePropTsDsl extends TypeTsDsl<ts.TypeElement> {
   }
 
   /** Sets the property type. */
-  type(type: string | ts.TypeNode): this {
-    this.typeInput = type;
+  type(type: string | MaybeTsDsl<ts.TypeNode>): this {
+    this._type = type;
     return this;
   }
 
   /** Builds and returns the property signature. */
   $render(): ts.TypeElement {
-    if (!this.typeInput) {
+    if (!this._type) {
       throw new Error(`Type not specified for property '${this.name}'`);
     }
     return ts.factory.createPropertySignature(
       undefined,
       this.name,
       this.questionToken,
-      this.$type(this.typeInput),
+      this.$type(this._type),
     );
   }
 }
