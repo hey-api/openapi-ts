2b94fd5958ad56ca752d14eea2bc26ac844e4fe3
Theodor Steiner <theodor.steiner@lycorp.co.jp>
2025-11-20 10:22:24 +0900

    chore: commit generated examples

--- examples/openapi-ts-tanstack-vue-query/src/client/core/serverSentEvents.gen.ts
+++ examples/openapi-ts-tanstack-vue-query/src/client/core/serverSentEvents.gen.ts
@@ -202,15 +202,19 @@ export const createSseClient = <TData = unknown>({
                 }
               }
 
-              onSseEvent?.({
+              const baseSseEvent = {
                 data,
                 event: eventName,
-                id: lastEventId,
+                id: lastEventId
+              }
+
+              onSseEvent?.({
+                ...baseSseEvent,
                 retry: retryDelay
               })
 
               if (dataLines.length) {
-                yield data as any
+                yield baseSseEvent as any
               }
             }
           }
