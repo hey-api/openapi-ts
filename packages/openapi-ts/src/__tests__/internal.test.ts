22ca0a3e35d5bddba842876803b1fd6ebf09e0ec
Alexander Sagalovskiy <spikesagal@gmail.com>
2026-02-09 22:03:58 -0700

    fix: updated args order

--- packages/openapi-ts/src/__tests__/internal.test.ts
+++ packages/openapi-ts/src/__tests__/internal.test.ts
@@ -1,12 +1,8 @@
 describe('internal entry index', () => {
-  it(
-    'should be exported',
-    async () => {
-      const internal = await import('../internal');
-      expect(internal.getSpec).toBeDefined();
-      expect(internal.initConfigs).toBeDefined();
-      expect(internal.parseOpenApiSpec).toBeDefined();
-    },
-    { timeout: 10000 },
-  );
+  it('should be exported', { timeout: 10000 }, async () => {
+    const internal = await import('../internal');
+    expect(internal.getSpec).toBeDefined();
+    expect(internal.initConfigs).toBeDefined();
+    expect(internal.parseOpenApiSpec).toBeDefined();
+  });
 });
