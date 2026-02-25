c96263ecc70bae2a1b1833df6bd535f35efc3671
Vinay Raghu <vinay.raghu@whoop.com>
2026-02-25 13:28:46 -0500

    fix: convert Headers to plain object before request validation

--- packages/openapi-ts/src/plugins/@hey-api/client-ofetch/bundle/client.ts
+++ packages/openapi-ts/src/plugins/@hey-api/client-ofetch/bundle/client.ts
@@ -54,7 +54,10 @@ export const createClient = (config: Config = {}): Client => {
     }
 
     if (opts.requestValidator) {
-      await opts.requestValidator(opts);
+      await opts.requestValidator({
+        ...opts,
+        headers: Object.fromEntries(opts.headers.entries()),
+      });
     }
 
     if (opts.body !== undefined && opts.bodySerializer) {
