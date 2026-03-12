7e9e27e49e98308b8ef2833e53f21f81ca6ecda2
Lubos <lubos@heyapi.dev>
2026-03-12 20:33:51 +0100

    fix: replace js-yaml and dotenv dependencies

--- dev/openapi-python.config.ts
+++ dev/openapi-python.config.ts
@@ -1,11 +1,16 @@
-import 'dotenv/config';
-
+import fs from 'node:fs';
 import path from 'node:path';
+import { parseEnv } from 'node:util';
 
 import { defineConfig } from '@hey-api/openapi-python';
 
 import { getInput } from './inputs';
 import { getPreset } from './python/presets';
+
+process.env = {
+  ...process.env,
+  ...parseEnv(fs.readFileSync(path.resolve(__dirname, '.env'), 'utf-8')),
+};
 
 export default defineConfig(() => [
   {
