39cbb07dc30789891d907573a8ca8a941a08b45c
Lubos <lubos@heyapi.dev>
2026-03-15 20:16:36 +0100

    fix: export Plugins namespace

--- packages/openapi-ts/src/plugins/@faker-js/faker/index.ts
+++ packages/openapi-ts/src/plugins/@faker-js/faker/index.ts
@@ -1,2 +1,3 @@
 export { defaultConfig, defineConfig } from './config';
+export type { Resolvers as FakerJsFakerResolvers } from './resolvers/types';
 export type { FakerJsFakerPlugin } from './types';
