366364930d6daff5e27404b92e5547641b6ce388
Lubos <lubos@heyapi.dev>
2026-03-19 23:12:37 +0100

    chore: add contracts options

---
+++ packages/openapi-ts/src/plugins/@orpc/contract/contracts/index.ts
@@ -0,0 +1,4 @@
+export { resolveContracts } from './config';
+export { type ContractItem, createShell, source, toNode } from './node';
+export { resolveStrategy } from './resolve';
+export type { ContractsConfig, UserContractsConfig } from './types';
