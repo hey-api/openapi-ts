9aa052db1f2c6f8ea58b1bcade18d4966d6b7678
Rodrigo <rodrigo.martinez@escrol.es>
2026-03-20 14:39:27 +0100

    feat(nuxt): support isomorphic nitro client

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -44,7 +44,7 @@ export default defineNuxtModule<ModuleOptions>({
 
     const config = defu(options.config, {
       output: {
-        path: path.join(nuxt.options.buildDir, 'client'),
+        path: path.join(nuxt.options.rootDir, '.nuxt', 'heyapi', 'client'),
       },
       plugins: (options.config.plugins || []).some(
         (plugin: Required<UserConfig>['plugins'][number]) => {
@@ -56,8 +56,36 @@ export default defineNuxtModule<ModuleOptions>({
         : ['@hey-api/client-nuxt'],
     } satisfies Partial<UserConfig>) as UserConfig;
 
+    const serverPlugins = (options.config.plugins || []).filter(
+      (plugin: Required<UserConfig>['plugins'][number]) => {
+        const pluginName = typeof plugin === 'string' ? plugin : plugin.name;
+        return pluginName !== '@hey-api/client-nuxt';
+      },
+    );
+    if (
+      !serverPlugins.some(
+        (p: any) => (typeof p === 'string' ? p : p.name) === '@hey-api/client-ofetch',
+      )
+    ) {
+      serverPlugins.push('@hey-api/client-ofetch');
+    }
+
+    const serverConfig: UserConfig = {
+      ...options.config,
+      output: {
+        ...(typeof options.config.output === 'object' &&
+        options.config.output !== null &&
+        !Array.isArray(options.config.output)
+          ? options.config.output
+          : {}),
+        path: path.join(nuxt.options.rootDir, '.nuxt', 'heyapi', 'nitroClient'),
+      },
+      plugins: serverPlugins,
+    };
+
     if (nuxt.options._prepare) {
       config.watch = false;
+      serverConfig.watch = false;
     }
 
     const output = config.output instanceof Array ? config.output[0] : config.output;
@@ -66,15 +94,31 @@ export default defineNuxtModule<ModuleOptions>({
       typeof output === 'string' ? output : (output?.path ?? ''),
     );
 
+    const serverOutput =
+      serverConfig.output instanceof Array ? serverConfig.output[0] : serverConfig.output;
+    const serverFolder = path.resolve(
+      nuxt.options.rootDir,
+      typeof serverOutput === 'string' ? serverOutput : (serverOutput?.path ?? ''),
+    );
+
     nuxt.options.alias[options.alias!] = folder;
 
+    nuxt.options.nitro = nuxt.options.nitro || {};
+    nuxt.options.nitro.alias = nuxt.options.nitro.alias || {};
+    nuxt.options.nitro.alias[options.alias!] = serverFolder;
+
+    nuxt.options.build.transpile.push(folder, serverFolder, options.alias!);
+    nuxt.options.nitro.externals = nuxt.options.nitro.externals || {};
+    nuxt.options.nitro.externals.inline = nuxt.options.nitro.externals.inline || [];
+    nuxt.options.nitro.externals.inline.push(folder, serverFolder, options.alias!);
+
     nuxt.hooks.hookOnce('app:templates', async () => {
-      await createClient(config);
+      await Promise.all([createClient(config), createClient(serverConfig)]);
     });
 
     // auto-import enabled
     if (options.autoImport) {
-      await createClient(config);
+      await Promise.all([createClient(config), createClient(serverConfig)]);
       const typeImports = new Set<string>();
       const valueImports = new Set<string>();
       const files = findExports(fs.readFileSync(path.join(folder, 'index.ts'), 'utf-8'));
@@ -99,13 +143,28 @@ export default defineNuxtModule<ModuleOptions>({
         }
       }
 
-      const imports = [...[...typeImports].map((name) => ({ name, type: true })), ...valueImports];
+      const typeImportsArray = [...typeImports].map((name) => ({ name, type: true }));
+      const imports = [...typeImportsArray, ...valueImports];
 
       if (imports.length && options.alias) {
         addImportsSources({
           from: options.alias,
           imports,
         });
+
+        nuxt.options.nitro.imports = nuxt.options.nitro.imports || {};
+        nuxt.options.nitro.imports.imports = nuxt.options.nitro.imports.imports || [];
+        nuxt.options.nitro.imports.imports.push(
+          ...typeImportsArray.map((i) => ({
+            from: options.alias!,
+            name: i.name,
+            type: true,
+          })),
+          ...[...valueImports].map((name) => ({
+            from: options.alias!,
+            name,
+          })),
+        );
       }
     }
   },
