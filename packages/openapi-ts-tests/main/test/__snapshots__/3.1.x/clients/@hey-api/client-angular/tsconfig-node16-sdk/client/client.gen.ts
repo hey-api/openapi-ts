d455dd8bd457063c9964d4c9b184fac164fdf0e0
Tom van de Velde <vdv@dnco.com>
2026-03-27 17:44:39 +0000

    chore: update snapshots

--- packages/openapi-ts-tests/main/test/__snapshots__/3.1.x/clients/@hey-api/client-angular/tsconfig-node16-sdk/client/client.gen.ts
+++ packages/openapi-ts-tests/main/test/__snapshots__/3.1.x/clients/@hey-api/client-angular/tsconfig-node16-sdk/client/client.gen.ts
@@ -97,7 +97,14 @@ export const createClient = (config: Config = {}): Client => {
     return { opts, req, url };
   };
 
-  const beforeRequest = async (options: RequestOptions) => {
+  const beforeRequest = async <
+    TData = unknown,
+    TResponseStyle extends ResponseStyle = 'fields',
+    ThrowOnError extends boolean = boolean,
+    Url extends string = string,
+  >(
+    options: RequestOptions<TData, TResponseStyle, ThrowOnError, Url>,
+  ) => {
     const { opts, req, url } = requestOptions(options);
 
     if (opts.security) {
@@ -115,7 +122,6 @@ export const createClient = (config: Config = {}): Client => {
   };
 
   const request: Client['request'] = async (options) => {
-    // @ts-expect-error
     const { opts, req: initialReq } = await beforeRequest(options);
 
     let req = initialReq;
