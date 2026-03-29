be397e2a371043b587cb9f8d35ce95be40e89366
Lubos <lubos@heyapi.dev>
2026-03-29 23:04:25 +0200

    chore: support overriding all arguments

--- dev/playground.ts
+++ dev/playground.ts
@@ -1,10 +1,12 @@
 import type { DefinePlugin, IR } from '@hey-api/openapi-ts';
+import { fromOpenApi } from '@msw/source/open-api';
 import { setupServer } from 'msw/node';
 
 import { client } from './gen/typescript/client.gen';
 // import { createOpencode } from '@opencode-ai/sdk';
-import { createMswHandlers, handleTuiPublish } from './gen/typescript/msw.gen';
+import { createMswHandlers } from './gen/typescript/msw.gen';
 import { OpenCode } from './gen/typescript/sdk.gen';
+import spec from './opencode.json';
 
 type MyPluginConfig = { readonly name: 'myplugin' };
 type MyPlugin = DefinePlugin<MyPluginConfig>;
@@ -19,30 +21,65 @@ export const handler: MyPlugin['Handler'] = ({ plugin }) => {
   });
 };
 
+const sourceHandlers = await fromOpenApi({
+  ...(spec as any),
+  basePath: 'https://api.example.com',
+});
 const handlers = createMswHandlers({
   baseUrl: 'https://api.example.com',
 });
 
 const server = setupServer(
-  // ...handlers.all({
-  //   // overrides: {
-  //   //   tuiPublish: {
-  //   //     result: true,
-  //   //   },
-  //   // },
-  // }),
-  handleTuiPublish({
-    result: false,
+  // ...sourceHandlers,
+  ...handlers.all({
+    one: {
+      // projectUpdate(info) {
+      //   console.log('Received request for projectUpdate with info:', info);
+      // },
+      projectUpdate: [
+        (info) => {
+          console.log('Received request for projectUpdate with info:', info);
+        },
+        // {
+        //   result: {
+        //     id: '123',
+        //     name: 'Updated Project Name',
+        //     time: {
+        //       created: 1678900000000,
+        //       updated: 1678900000000,
+        //     },
+        //     worktree: 'main',
+        //     icon: {
+        //       url: 'https://example.com/icon.png',
+        //       color: 'blue',
+        //     },
+        //     vcs: 'git',
+        //   },
+        //   status: 200,
+        // },
+        {
+          // baseUrl: 'https://api.example.com',
+        },
+      ],
+      // projectUpdate: {
+      //   result: {
+      //     id: '123',
+      //     name: 'Updated Project Name',
+      //     time: {
+      //       created: 1678900000000,
+      //       updated: 1678900000000,
+      //     },
+      //     worktree: 'main',
+      //     icon: {
+      //       url: 'https://example.com/icon.png',
+      //       color: 'blue',
+      //     },
+      //     vcs: 'git',
+      //   },
+      // },
+    },
   }),
-  handlers.one.tuiPublish(
-    {
-      result: false,
-      // status: 200,
-    },
-    {
-      // ...
-    },
-  ),
+  // handlers.one.projectUpdate(),
 );
 server.listen();
 
@@ -53,17 +90,27 @@ async function run() {
     baseUrl: 'https://api.example.com',
   });
   const sdk = new OpenCode({ client });
-  const published = await sdk.tui.publish({
-    body: {
-      properties: {
-        message: 'Hello from Hey API OpenAPI TypeScript Playground!',
-        variant: 'success',
-      },
-      type: 'tui.toast.show',
-    },
+  // const published = await sdk.tui.publish({
+  //   body: {
+  //     properties: {
+  //       message: 'Hello from Hey API OpenAPI TypeScript Playground!',
+  //       variant: 'success',
+  //     },
+  //     type: 'tui.toast.show',
+  //   },
+  //   directory: 'main',
+  // });
+  // console.log('Published:', published.data, published.error);
+  const project = await sdk.project.update({
+    projectID: '123',
     directory: 'main',
+    icon: {
+      color: 'blue',
+      url: 'https://example.com/icon.png',
+    },
+    name: 'Updated Project Name',
   });
-  console.log('Published:', published.data, published.error);
+  console.log('Updated Project:', project.data, project.error);
 }
 
 run();
