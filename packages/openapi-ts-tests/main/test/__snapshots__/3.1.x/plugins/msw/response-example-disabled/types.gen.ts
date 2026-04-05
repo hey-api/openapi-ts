63462ac3d683cf386f6f9fe4b1c1acbf7605d607
Lubos <lubos@heyapi.dev>
2026-04-05 23:22:37 +0200

    chore: improve baseUrl types

--- packages/openapi-ts-tests/main/test/__snapshots__/3.1.x/plugins/msw/response-example-disabled/types.gen.ts
+++ packages/openapi-ts-tests/main/test/__snapshots__/3.1.x/plugins/msw/response-example-disabled/types.gen.ts
@@ -48,3 +48,21 @@ export type PostFooResponses = {
 };
 
 export type PostFooResponse = PostFooResponses[keyof PostFooResponses];
+
+export type PutFooData = {
+    body?: never;
+    path?: never;
+    query?: never;
+    url: '/foo';
+};
+
+export type PutFooResponses = {
+    /**
+     * OK
+     */
+    200: {
+        name?: string;
+    };
+};
+
+export type PutFooResponse = PutFooResponses[keyof PutFooResponses];
