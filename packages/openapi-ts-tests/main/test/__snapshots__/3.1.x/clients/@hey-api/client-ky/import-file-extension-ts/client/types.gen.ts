e89531fa3aeb9833879daf2c93a4cd05cf060640
inas sarhan <inassirhan@gmail.com>
2026-04-14 14:35:54 +0300

    fix(client): apply headers narrowing to next, ky, ofetch, angular

--- packages/openapi-ts-tests/main/test/__snapshots__/3.1.x/clients/@hey-api/client-ky/import-file-extension-ts/client/types.gen.ts
+++ packages/openapi-ts-tests/main/test/__snapshots__/3.1.x/clients/@hey-api/client-ky/import-file-extension-ts/client/types.gen.ts
@@ -125,6 +125,7 @@ export interface ResolvedRequestOptions<
   ThrowOnError extends boolean = boolean,
   Url extends string = string,
 > extends RequestOptions<unknown, TResponseStyle, ThrowOnError, Url> {
+  headers: Headers;
   serializedBody?: string;
 }
 
