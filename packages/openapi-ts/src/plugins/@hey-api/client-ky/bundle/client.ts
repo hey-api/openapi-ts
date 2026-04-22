e72ab285ea67ee8feb61a8a4a4b794fccf3d87d6
SukkaW <isukkaw@gmail.com>
2026-04-23 04:12:33 +0800

    fix: properly execute error interceptors

--- packages/openapi-ts/src/plugins/@hey-api/client-ky/bundle/client.ts
+++ packages/openapi-ts/src/plugins/@hey-api/client-ky/bundle/client.ts
@@ -273,7 +273,7 @@ export const createClient = (config: Config = {}): Client => {
 
       for (const fn of interceptors.error.fns) {
         if (fn) {
-          finalError = (await fn(error, response, request, options as any)) as string;
+          finalError = (await fn(finalError, response, request, options as any)) as string;
         }
       }
 
