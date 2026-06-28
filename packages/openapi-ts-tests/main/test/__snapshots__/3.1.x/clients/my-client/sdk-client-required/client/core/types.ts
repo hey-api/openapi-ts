84a2d36aa7ed0830c0df29f3d52764140da34fe4
Lubos <lubos@heyapi.dev>
2026-06-28 23:59:27 +0800

    test: update snapshots

--- packages/openapi-ts-tests/main/test/__snapshots__/3.1.x/clients/my-client/sdk-client-required/client/core/types.ts
+++ packages/openapi-ts-tests/main/test/__snapshots__/3.1.x/clients/my-client/sdk-client-required/client/core/types.ts
@@ -76,7 +76,6 @@ export interface Config {
 /**
  * Arbitrary metadata passed through the `meta` request option.
  */
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface ClientMeta {}
 
 /**
